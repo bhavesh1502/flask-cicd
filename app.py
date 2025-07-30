9171c8e7521d81a5acbb65634df8d2e546744c11
Bhavesh Addala <116192735+bhavesh1502@users.noreply.github.com>
2025-07-30 17:56:25 +0530

    modified msg

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello, CICD World!!"
+def hello():
+    return "Hello from EC2!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
